e66686f59154485cf99762cba7e1e68591c51820
Semen <semenggde@gmail.com>
2022-03-25 19:43:05 +0100

    README.md

--- helloWorld.py
+++ helloWorld.py
@@ -1,23 +1,30 @@
-print( "Start simulator (SITL)")
+print("Start simulator (SITL)")
 import dronekit_sitl
+
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
 
 # Import DroneKit-Python
-from dronekit import connect, VehicleMode
+from dronekit import connect, VehicleMode, Vehicle
 
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+vehicle: Vehicle = connect(connection_string, wait_ready=True)
 
 # Get some vehicle attributes (state)
-print ("Get some vehicle attribute values:")
-print (" GPS: %s" % vehicle.gps_0)
-print (" Battery: %s" % vehicle.battery)
-print (" Last Heartbeat: %s" % vehicle.last_heartbeat)
-print ( " Is Armable?: %s" % vehicle.is_armable)
-print ( " System status: %s" % vehicle.system_status.state)
-print( " Mode: %s" % vehicle.mode.name )   # settable
+print("Get some vehicle attribute values:")
+print(" GPS: %s" % vehicle.gps_0)
+print(" Battery: %s" % vehicle.battery)
+print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+print(" Is Armable?: %s" % vehicle.is_armable)
+print(" System status: %s" % vehicle.system_status.state)
+print(" Mode: %s" % vehicle.mode.name)  # settable
+
+latitude = vehicle.location.global_relative_frame.lat
+longitude = vehicle.location.global_relative_frame.lon
+
+print(latitude)
+print(longitude)
 
 # Close vehicle object before exiting script
 vehicle.close()
